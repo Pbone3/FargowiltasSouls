53cba0ff4ff3e4e2954c20f4fc02249d29b8c302
Pbone3 <pbone785@gmail.com>
2021-06-07 17:14:02 -0500

    Round one of projectiles

--- Items/Weapons/BossDrops/DarkMageBook.cs
+++ Items/Weapons/BossDrops/DarkMageBook.cs
@@ -1,4 +1,7 @@
-﻿using Terraria.ID;
+﻿using FargowiltasSouls.Projectiles.BossWeapons.DarkMage;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace FargowiltasSouls.Items.Weapons.BossDrops
@@ -8,27 +11,42 @@ namespace FargowiltasSouls.Items.Weapons.BossDrops
         public override void SetStaticDefaults()
         {
             DisplayName.SetDefault("Old One's Tome");
-            Tooltip.SetDefault("'The repurposed mount of a defeated foe...'");
+            Tooltip.SetDefault(
+                "Shoots out three sentient page-firing books" +
+              "\nRepeated hits summon a short-lived skeleton for extra damage" +
+              "\n'The repurposed mount of a defeated foe...'");
         }
 
         public override void SetDefaults()
         {
-            item.damage = 22;
+            item.damage = 16;
             item.magic = true;
             item.width = 28;
             item.height = 30;
-            item.useTime = 28;
-            item.useAnimation = 28;
+            item.useTime = 30;
+            item.useAnimation = 30;
             item.useStyle = ItemUseStyleID.HoldingOut;
             item.noMelee = true;
             item.knockBack = 1;
             item.value = 45000;
             item.rare = ItemRarityID.Orange;
             item.mana = 14;
-            item.UseSound = SoundID.Item8;
+            item.UseSound = SoundID.DD2_DarkMageAttack.WithVolume(0.5f);
             item.autoReuse = true;
             item.shoot = ProjectileID.WoodenArrowFriendly;
-            item.shootSpeed = 8f;
+            item.shootSpeed = 6f;
+        }
+
+        public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+        {
+            const int bookAmount = 3;
+            for (int i = 0; i < bookAmount; i++)
+            {
+                Vector2 velocity = new Vector2(speedX, speedY).RotatedByRandom(MathHelper.PiOver4) * (1 + Main.rand.NextFloat(0.5f));
+                Projectile.NewProjectile(position, velocity, ModContent.ProjectileType<DarkMageBookPro1>(), damage, 0f, player.whoAmI);
+            }
+
+            return false;
         }
     }
 }
---
+++ Projectiles/BossWeapons/DarkMage/DarkMageBookPro1.cs
@@ -0,0 +1,83 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria.ID;
+
+namespace FargowiltasSouls.Projectiles.BossWeapons.DarkMage
+{
+    public class DarkMageBookPro1 : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            projectile.magic = true;
+            projectile.width = 10;
+            projectile.height = 10;
+            projectile.friendly = true;
+            projectile.penetrate = -1;
+            projectile.alpha = 255;
+        }
+
+        public float Counter { get => projectile.ai[0]; set => projectile.ai[0] = value; }
+
+        public override void AI()
+        {
+            projectile.alpha -= 30;
+            if (projectile.alpha < 0)
+                projectile.alpha = 0;
+
+            if (projectile.damage > 0)
+            {
+                // Safe damage it was spawned with
+                projectile.ai[1] = projectile.damage;
+                projectile.damage = 0;
+            }
+
+            // Slow down
+            projectile.velocity *= 0.95f;
+
+            // Twice, at 32 ticks and right before death
+            if (Counter++ % 16 == 0 && Counter > 17)
+            {
+                // Shoot
+                Projectile.NewProjectile(projectile.Center, projectile.DirectionTo(Main.MouseWorld) * 8, ProjectileID.BookStaffShot, (int)projectile.ai[1], 1f, projectile.owner);
+            }
+
+            if (Counter >= 50)
+            {
+                // Die
+                projectile.Kill();
+
+                // Dust
+                Gore.NewGore(projectile.Center, default, GoreID.PageScrap);
+
+                if (Main.rand.NextBool(4))
+                    Gore.NewGore(projectile.Center, default, GoreID.Pages);
+                if (Main.rand.NextBool(5))
+                    Gore.NewGore(projectile.Center, default, GoreID.PageScrap);
+
+                for (int i = 0; i < 7; i++)
+                {
+                    Dust dust = Dust.NewDustDirect(projectile.position, projectile.width + 4, projectile.height + 4, 86); // DustID.AmethystBolt
+                    dust.noGravity = true;
+                }
+            }
+
+            if (projectile.owner == Main.myPlayer)
+            {
+                // Rotate to cursor pos
+                projectile.rotation = Utils.AngleLerp(projectile.rotation, projectile.DirectionTo(Main.MouseWorld).ToRotation(), 0.3f);
+            }
+        }
+
+        public override bool OnTileCollide(Vector2 oldVelocity) => false;
+
+        public override bool PreDraw(SpriteBatch spriteBatch, Color lightColor)
+        {
+            Texture2D texture = Main.projectileTexture[projectile.type];
+            Vector2 position = projectile.position + projectile.Size * 0.5f;
+            spriteBatch.Draw(texture, position - Main.screenPosition, null, Color.White, projectile.rotation, texture.Size() * 0.5f, 1f, SpriteEffects.None, 0f);
+            return false;
+        }
+    }
+}
